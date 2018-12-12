72d497aa2bc4c0bb91bf1cfdf1d07400e841236f
KarloDerEchte <zschorlichjohann@gmail.com>
2018-12-12 19:34:42 +0100

    Configurationg index.json

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-// Das ist vom Nebenbot den ich mit Github connecten möchte
-//  done
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const fs = require('fs');
+
+const banlist = require('./config/banwords.json');
+
+let token;
+
+if (fs.existsSync('./config/token.json')) {
+  token = require('./config/token.json');
+  client.login(token.token);
+} else {
+  client.login(process.env.BOT_TOKEN);
+}
+client.on('ready', () => {
+  console.log(`Bot Erfolgreich auf dem Namen ${client.user.tag} gestartet!`);
+});
+
+client.on('message', async (msg) => {
+  if (msg.author.bot) return;
+{
+    if ()
+}
