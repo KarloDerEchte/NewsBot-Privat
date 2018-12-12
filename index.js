6dd5f3e68b6ece38ab985db6588db24476571ef6
KarloDerEchte <zschorlichjohann@gmail.com>
2018-12-12 22:03:33 +0100

    NEW NEWS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 const fs = require('fs');
 
-const banlist = require('./config/banwords.json');
+//const banlist = require('./config/banwords.json');
 
 let token;
 
@@ -20,6 +20,8 @@ client.on('ready', () => {
 
 client.on('message', async (msg) => {
   if (msg.author.bot) return;
-{
-    if ()
-}
+
+  if (msg.content === 'NEWS') {
+      msg.channel.send('```In Straßburg gab es am Dienstagabend Schüsse bei einem Weihnachtsmarkt. Mehrere Menschen starben, es gibt zahlreiche Verletzte. Der Täter ist weiter auf der Flucht, Frankreich ruft die höchste Terrorwarnstufe aus.\n Am Dienstagabend gab es Schüsse in der Innenstadt von Straßburg.\n Drei Menschen werden aus dem Leben gerissen, zudem mehr als ein Dutzend verletzt. Laut Polizei ist der Täter noch flüchtig, 350 Sicherheitskräfte sind an der Fahndung beteiligt. In Frankreich gilt nun die höchste Terrorwarnstufe.Soldaten haben den mutmaßlichen Täter angeschossen. Er war den französischen Behörden als potenzieller Gefährder bekannt.In Deutschland saß der mutmaßliche Angreifer ebenso wie in Frankreich und der Schweiz wegen Einbrüchen im Gefängnias.\n***QUELLE:https://www.merkur.de/politik/strssburg-frankreich-tote-bei-anschlag-auf-weihnachtsmarkt-merkel-aeussert-sich-zr-10846090.html***```')
+  }
+});
